a364ccd7560d8ca0f610ed0760429d356edc2c37
Giles Cope <gilescope@gmail.com>
2016-05-30 22:55:41 +0100

    Testing signup flow - example of using click.label

--- Tests.js
+++ Tests.js
@@ -15,6 +15,42 @@ casper.test.begin('Front page tests', 1, function(test) {
         test.done();
     });
 });
+
+casper.test.begin('Can book a free taster', 5, function(test) {
+    casper.start("http://crossfitblackwater.com", function () {
+        "use strict";
+        test.assertTitle("CrossFit Blackwater, Witham's Premier CrossFit box. Witham, Essex.");
+
+        this.clickLabel('Programs','a');
+        this.clickLabel('CrossFit Taster','a');
+    }, true);
+
+    casper.then(function() {
+        test.assertEquals('https://crossfitblackwater.com/free-gym-taster-witham/',this.getCurrentUrl());
+
+        this.clickLabel('Book my free taster!', 'span');
+    });
+
+    casper.then(function() {
+        test.assertEquals('https://goteamup.com/p/174765-blackwater-health-and-fit/auth/9096/',this.getCurrentUrl());
+        this.back();
+    });
+
+    casper.then(function() {
+        test.assertEquals('https://crossfitblackwater.com/free-gym-taster-witham/',this.getCurrentUrl());
+
+        this.clickLabel('Already sold? See Membership Deals', 'span');
+    });
+
+    casper.then(function() {
+        test.assertEquals('https://crossfitblackwater.com/membership/',this.getCurrentUrl());
+    });
+
+    casper.run(function(){
+        test.done();
+    });
+});
+
 casper.test.begin('Membership tests', 2, function(test) {
 
     casper.start("http://crossfitblackwater.com/membership", function(){
